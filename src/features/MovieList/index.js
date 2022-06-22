9a9a30e50d49dbbf77244e600bd40ffe77b97436
Damian Michalowski <dowski888@gmail.com>
2022-06-22 21:50:40 +0200

    move MovieList from common to features

--- src/features/MovieList/index.js
+++ src/features/MovieList/index.js
@@ -1,3 +1,16 @@
+import { MovieTile } from "./MovieTile";
+
 export const MovieList = () => {
-    return <h1>Popular Movies</h1>
-}
+    return (
+        <>
+            <h1>Popular movies</h1>
+            <MovieTile
+                title={"Mulan"}
+                year={"2020"}
+                genre={"Action"} //na podstawie tego w przyszłości będzie renderować te boksy z gatunkami
+                rate={"7,8"}
+                votes={"35"}
+            />
+        </>
+    )
+};
