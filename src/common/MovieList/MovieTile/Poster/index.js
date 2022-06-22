1cec1421b02a84c46fe691cc025a54ac497029bb
Damian Michalowski <dowski888@gmail.com>
2022-06-22 16:01:36 +0200

    refactoringMovieTile,deletePositionAbsolut,flexadd

--- src/common/MovieList/MovieTile/Poster/index.js
+++
@@ -1,9 +0,0 @@
-import { Wrapper, Image } from "./styled";
-import CameraIcon from "../../../../images/camera.svg";
-export const Poster = () => {
-    return (
-        <Wrapper>
-            <Image src={CameraIcon} />
-        </Wrapper>
-    )
-};
--- src/common/MovieList/MovieTile/index.js
+++ src/common/MovieList/MovieTile/index.js
@@ -1,14 +1,42 @@
-import Container from "./Container";
-import { Poster } from "./Poster";
-import { Description } from "./Description";
-import { Rating } from "./Rating";
+import Star from "../../../images/star.svg"
+import Camera from "../../../images/camera.svg"
+import {
+    Container,
+    DescriptionContainer,
+    TagsContainer,
+    Tag,
+    Title,
+    Year,
+    RatingContainer,
+    CameraIcon,
+    Poster,
+    StarIcon,
+    Rate,
+    Votes
+} from "./styled";
 
 function MovieTile() {
     return (
         <Container>
-            <Poster />
-            <Description />
-            <Rating />
+            <Poster>
+                <CameraIcon src={Camera} />
+            </Poster>
+
+            <DescriptionContainer>
+                <Title> Mulan </Title>
+                <Year> 2020 </Year>
+                <TagsContainer>
+                    <Tag> Action </Tag>
+                    <Tag> Adventure </Tag>
+                    <Tag> Drama </Tag>
+                </TagsContainer>
+            </DescriptionContainer>
+
+            <RatingContainer>
+                <StarIcon src={Star} />
+                <Rate>7,8</Rate>
+                <Votes>35 votes</Votes>
+            </RatingContainer>
         </Container>
     )
 }
