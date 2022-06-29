88c9b4d1b84d494d8a670d777c4834b7df44887e
Damian Michalowski <dowski888@gmail.com>
2022-06-29 16:46:40 +0200

    responsive pagination

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,33 +1,81 @@
 import { PaginationContainer, FirstPrevWrapper, PagesWrapper, NextLastWrapper, First, Previous, Next, Last, StyledNextIcon, StyledPrevIcon, CurrentPage } from "./styled";
+import { useState, useEffect } from "react";
+
+function useWindowSize() {
+    const [size, setSize] = useState([ window.innerWidth]);
+    useEffect(() => {
+        const handleResize = () => {
+            setSize([window.innerWidth]);
+        };
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, []);
+    return size;
+};
 
 export const Pagination = () => {
-    return (
-        <PaginationContainer>
-            <FirstPrevWrapper>
-                <First>
-                    <StyledPrevIcon />
-                    First
-                </First>
-                <Previous>
-                    <StyledPrevIcon />
-                    Previous
-                </Previous>
-            </FirstPrevWrapper>
+    const [width] = useWindowSize();
+    const number = 767;
+    if(width <= number) {
+        return (
+            <PaginationContainer>
+                <FirstPrevWrapper>
+                    <First>
+                        <StyledPrevIcon />
+                        <StyledPrevIcon />
+                    </First>
+                    <Previous>
+                        <StyledPrevIcon />
+                    </Previous>
+                </FirstPrevWrapper>
+    
+                <PagesWrapper>
+                    Page <CurrentPage>1</CurrentPage> of <CurrentPage>500</CurrentPage>
+                </PagesWrapper>
+    
+                <NextLastWrapper>
+                    <Next>
+                        <StyledNextIcon />
+                    </Next>
+                    <Last>
+                        <StyledNextIcon />
+                        <StyledNextIcon />
+                    </Last>
+                </NextLastWrapper>
+            </PaginationContainer>
+        )
+    }else {
 
-            <PagesWrapper>
-                Page <CurrentPage>1</CurrentPage> of <CurrentPage>500</CurrentPage>
-            </PagesWrapper>
-
-            <NextLastWrapper>
-                <Next>
-                    Next
-                    <StyledNextIcon />
-                </Next>
-                <Last>
-                    Last
-                    <StyledNextIcon />
-                </Last>
-            </NextLastWrapper>
-        </PaginationContainer>
-    )
+        return (
+            <PaginationContainer>
+                <FirstPrevWrapper>
+                    <First>
+                        <StyledPrevIcon />
+                        First
+                    </First>
+                    <Previous>
+                        <StyledPrevIcon />
+                        Previous
+                    </Previous>
+                </FirstPrevWrapper>
+    
+                <PagesWrapper>
+                    Page <CurrentPage>1</CurrentPage> of <CurrentPage>500</CurrentPage>
+                </PagesWrapper>
+    
+                <NextLastWrapper>
+                    <Next>
+                        Next
+                        <StyledNextIcon />
+                    </Next>
+                    <Last>
+                        Last
+                        <StyledNextIcon />
+                    </Last>
+                </NextLastWrapper>
+            </PaginationContainer>
+        )
+    }
 };
--- src/common/Pagination/styled.js
+++ src/common/Pagination/styled.js
@@ -3,15 +3,20 @@ import { ReactComponent as prev } from "../../images/prev.svg"
 import { ReactComponent as next } from "../../images/next.svg"
 
 export const PaginationContainer = styled.footer`
-    width: 525px;
-    height: 36px;
+    max-width: 525px;
+    max-height: 36px;
     margin: 40px auto 103px;
-
     display: flex;
     flex-direction: row;
     align-items: center;
-    padding: 0px;
+    justify-content: center;
     gap: 24px;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+        gap: 8px;
+        max-height: 23px;
+        margin: 32.5px auto 31.5px;
+    }
 `;
 
 export const FirstPrevWrapper = styled.div`
@@ -20,40 +25,38 @@ export const FirstPrevWrapper = styled.div`
     align-items: center;
     padding: 0px;
     gap: 12px;
+    color: #18181B;
+    font-size: 14px;
+    line-height: 140%;
 `;
 
 export const First = styled.span`
+    padding: 8px 16px;
+    border-radius: 5px;
     display: flex;
     flex-direction: row;
     align-items: center;
-    padding: 8px 16px;
     gap: 8px;
-
-    width: 76px;
     background: #E4E6F0;
-    border-radius: 5px;
 
-
-    color: #18181B;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 140%;
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+        gap: 4px;
+        padding: 8px 12px;
+    }
 `;
 
 export const Previous = styled.span`
-    width: 106px;
     display: flex;
     flex-direction: row;
     align-items: center;
     padding: 8px 16px;
+    border-radius: 5px;
     gap: 8px;
     background: #E4E6F0;
-    border-radius: 5px;
-
-    color: #18181B;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 140%;
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+        gap: 4px;
+        padding: 8px 12px;
+    }
 `;
 
 export const PagesWrapper = styled.footer`
@@ -62,11 +65,15 @@ export const PagesWrapper = styled.footer`
     align-items: center;
     padding: 0px;
     gap: 8px;
-
     color: #7E839A;
-    font-weight: 400;
     font-size: 16px;
     line-height: 150%;
+
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+        font-size: 10px;
+        line-height: 24px;
+        gap: 2px;
+    }
 `;
 
 export const CurrentPage = styled.span`
@@ -80,10 +87,26 @@ export const NextLastWrapper = styled.footer`
     align-items: center;
     padding: 0px;
     gap: 12px;
+    color: #18181B;
+    font-size: 14px;
+    line-height: 140%;
 `;
 
 export const Next = styled.span`
-    width: 78px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 8px 16px;
+    gap: 8px;
+    background: #D6E4FF;
+    border-radius: 5px;
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+        gap: 4px;
+        padding: 8px 12px;
+    }
+`;
+
+export const Last = styled.span`
     display: flex;
     flex-direction: row;
     align-items: center;
@@ -92,33 +115,23 @@ export const Next = styled.span`
     background: #D6E4FF;
     border-radius: 5px;
 
-    color: #18181B;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 140%;
-`;
-
-export const Last = styled.span`
-   width: 75px;
-   display: flex;
-    flex-direction: row;
-    align-items: center;
-    padding: 8px 16px;
-    gap: 8px;
-    background: #D6E4FF;
-    border-radius: 5px;
-
-    color: #18181B;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 140%;
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+        gap: 4px;
+        padding: 8px 12px;
+    }
 `;
 
 
 export const StyledNextIcon = styled(next)`
-  
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    width: 5px;
+height: 8px;
+    }
 `;
 
 export const StyledPrevIcon = styled(prev)`
-  
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    width: 5px;
+height: 8px;
+    }
 `;
