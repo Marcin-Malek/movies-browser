ef515f0c2269bd35f8dbaba39a599b5f3cf071d3
Damian Michalowski <dowski888@gmail.com>
2022-06-30 01:12:48 +0200

    pagination render fix

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -1,8 +1,9 @@
 import { PaginationContainer, FirstPrevWrapper, PagesWrapper, NextLastWrapper, First, Previous, Next, Last, StyledNextIcon, StyledPrevIcon, CurrentPage } from "./styled";
 import { useState, useEffect } from "react";
+import { theme } from "../../app/theme.js";
 
-function useWindowSize() {
-    const [size, setSize] = useState([ window.innerWidth]);
+export const useWindowSize = () => {
+    const [size, setSize] = useState([window.innerWidth]);
     useEffect(() => {
         const handleResize = () => {
             setSize([window.innerWidth]);
@@ -17,65 +18,34 @@ function useWindowSize() {
 
 export const Pagination = () => {
     const [width] = useWindowSize();
-    const number = 767;
-    if(width <= number) {
-        return (
-            <PaginationContainer>
-                <FirstPrevWrapper>
-                    <First>
-                        <StyledPrevIcon />
-                        <StyledPrevIcon />
-                    </First>
-                    <Previous>
-                        <StyledPrevIcon />
-                    </Previous>
-                </FirstPrevWrapper>
-    
-                <PagesWrapper>
-                    Page <CurrentPage>1</CurrentPage> of <CurrentPage>500</CurrentPage>
-                </PagesWrapper>
-    
-                <NextLastWrapper>
-                    <Next>
-                        <StyledNextIcon />
-                    </Next>
-                    <Last>
-                        <StyledNextIcon />
-                        <StyledNextIcon />
-                    </Last>
-                </NextLastWrapper>
-            </PaginationContainer>
-        )
-    }else {
 
-        return (
-            <PaginationContainer>
-                <FirstPrevWrapper>
-                    <First>
-                        <StyledPrevIcon />
-                        First
-                    </First>
-                    <Previous>
-                        <StyledPrevIcon />
-                        Previous
-                    </Previous>
-                </FirstPrevWrapper>
-    
-                <PagesWrapper>
-                    Page <CurrentPage>1</CurrentPage> of <CurrentPage>500</CurrentPage>
-                </PagesWrapper>
-    
-                <NextLastWrapper>
-                    <Next>
-                        Next
-                        <StyledNextIcon />
-                    </Next>
-                    <Last>
-                        Last
-                        <StyledNextIcon />
-                    </Last>
-                </NextLastWrapper>
-            </PaginationContainer>
-        )
-    }
+    return (
+        <PaginationContainer>
+            <FirstPrevWrapper>
+                <First>
+                    <StyledPrevIcon />
+                    {(width <= theme.breakpoint.mobile) ? <StyledPrevIcon /> : "First"}
+                </First>
+                <Previous>
+                    <StyledPrevIcon />
+                    {width <= theme.breakpoint.mobile ? "" : "Previous"}
+                </Previous>
+            </FirstPrevWrapper>
+
+            <PagesWrapper>
+                Page <CurrentPage>1</CurrentPage> of <CurrentPage>500</CurrentPage>
+            </PagesWrapper>
+
+            <NextLastWrapper>
+                <Next>
+                    {width <= theme.breakpoint.mobile ? "" : "Next"}
+                    <StyledNextIcon />
+                </Next>
+                <Last>
+                    {width <= theme.breakpoint.mobile ? <StyledNextIcon /> : "Last"}
+                    <StyledNextIcon />
+                </Last>
+            </NextLastWrapper>
+        </PaginationContainer>
+    )
 };
