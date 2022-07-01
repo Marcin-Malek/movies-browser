0374989d75e2e468ed4ecafd4aca0f6c73fedd27
Damian Michalowski <dowski888@gmail.com>
2022-07-01 17:44:21 +0200

    MovieTile main copy

---
+++ src/features/MovieList/MovieTile/index.js
@@ -0,0 +1,42 @@
+// import Camera from "../../../images/camera.svg";
+import Poster from "../../../images/MulanPoster.jpg";
+
+import {
+    Content,
+    Wrapper,
+    TagsContainer,
+    Tag,
+    Title,
+    Year,
+    RatingContainer,
+    Img,
+    Rate,
+    Votes,
+    StyledStar,
+    DescriptionContainer,
+} from "./styled";
+
+export const MovieTile = ({ title, year, tags, rate, votes }) => {
+    return (
+        <Content>
+            <Img src={Poster} />
+            <Wrapper>
+                <DescriptionContainer>
+                    <Title> {title} </Title>
+                    <Year> {year} </Year>
+                    <TagsContainer>
+                        {tags.map((tag, tagIndex) => (
+                            <Tag key={tagIndex}>{tag}</Tag>
+                        ))}
+                    </TagsContainer>
+                </DescriptionContainer>
+
+                <RatingContainer>
+                    <StyledStar />
+                    <Rate>{rate}</Rate>
+                    <Votes>{votes} votes</Votes>
+                </RatingContainer>
+            </Wrapper>
+        </Content>
+    )
+};
---
+++ src/features/MovieList/index.js
@@ -0,0 +1,48 @@
+import { MovieTile } from "./MovieTile";
+import { Content, Title, Wrapper } from "./styled";
+
+export const MovieList = () => {
+    return (
+
+        <Content>
+            <Title>Popular movies</Title>
+            <Wrapper>
+                <MovieTile
+                    title={"Mulan"}
+                    year={"2020"}
+                    tags={["Action", "Adventure", "Drama"]}
+                    rate={"7,8"}
+                    votes={"35"}
+                />
+                <MovieTile
+                    title={"Mulan"}
+                    year={"2020"}
+                    tags={[]}
+                    rate={"7,8"}
+                    votes={"35"}
+                />
+                <MovieTile
+                    title={"Mulan"}
+                    tags={["Action", "Drama"]}
+                    rate={"7,8"}
+                    votes={"35"}
+                />
+                <MovieTile
+                    title={"Mulan"}
+                    year={"2020"}
+                    tags={["Action", "Adventure", "Drama"]}
+                    rate={"7,8"}
+                    votes={"35"}
+                />
+                <MovieTile
+                    title={"Mulan"}
+                    year={"2020"}
+                    tags={["Action", "Adventure", "Drama"]}
+                    rate={"7,8"}
+                    votes={"35"}
+                />
+            </Wrapper>
+        </Content>
+
+    )
+};
