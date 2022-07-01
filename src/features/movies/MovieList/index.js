280ea6c316a741599800751d33f9a89d64050d2e
Marcin Malek <marcinmalek01@gmail.com>
2022-07-01 14:41:39 +0200

    dispatched fetching action in MovieList component, added conditional rendering based on fetchStatus, mapped results(not entirely)

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -1,48 +1,40 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchMovies, selectFetchStatus, selectMoviesList, } from "../moviesSlice";
 import { MovieTile } from "./MovieTile";
 import { Content, Title, Wrapper } from "./styled";
 
 export const MovieList = () => {
-    return (
+    const dispatch = useDispatch();
+    const fetchStatus = useSelector(selectFetchStatus);
+    const movies = useSelector(selectMoviesList);
 
-        <Content>
-            <Title>Popular movies</Title>
-            <Wrapper>
-                <MovieTile
-                    title={"Mulan"}
-                    year={"2020"}
-                    tags={["Action", "Adventure", "Drama"]}
-                    rate={"7,8"}
-                    votes={"35"}
-                />
-                <MovieTile
-                    title={"Mulan"}
-                    year={"2020"}
-                    tags={[]}
-                    rate={"7,8"}
-                    votes={"35"}
-                />
-                <MovieTile
-                    title={"Mulan"}
-                    tags={["Action", "Drama"]}
-                    rate={"7,8"}
-                    votes={"35"}
-                />
-                <MovieTile
-                    title={"Mulan"}
-                    year={"2020"}
-                    tags={["Action", "Adventure", "Drama"]}
-                    rate={"7,8"}
-                    votes={"35"}
-                />
-                <MovieTile
-                    title={"Mulan"}
-                    year={"2020"}
-                    tags={["Action", "Adventure", "Drama"]}
-                    rate={"7,8"}
-                    votes={"35"}
-                />
-            </Wrapper>
-        </Content>
+    useEffect(() => {
+        dispatch(fetchMovies());
+    }, [dispatch]);
 
-    )
+    switch (fetchStatus) {
+        case "completed":
+            const moviesData = movies.data
+            return (
+                <Content>
+                    <Title>Popular movies</Title>
+                    <Wrapper>
+                        {moviesData.results.map((movie) =>
+                            <MovieTile key={movie.id}
+                                title={movie.title}
+                                year={"2020"}
+                                tags={["Action", "Adventure", "Drama"]}
+                                rate={movie.vote_average}
+                                votes={movie.vote_count}
+                            />
+                        )}
+                    </Wrapper>
+                </Content>
+            );
+        case "error":
+            return (<span>error</span>)
+        default:
+            return (<span>loading</span>)
+    }
 };
