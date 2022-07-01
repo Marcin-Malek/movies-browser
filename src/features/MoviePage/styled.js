67b171126cbf185e762c0566ab4cfd0a4d3aca62
Marcin Malek <marcinmalek01@gmail.com>
2022-07-01 10:45:07 +0200

    code simplification

--- src/features/MoviePage/styled.js
+++ src/features/MoviePage/styled.js
@@ -1,13 +1,7 @@
 import styled from "styled-components";
 
 export const MainWrapper = styled.main`
-    max-width: 1368px;
+    max-width: 1400px;
     margin: 64px auto 336px;
-
-    // This keeps minimal 16px margins on sides while 
-    // using margin: auto; to center content
-    @media (max-width: 1400px) {
-        margin-left: 16px;
-        margin-right: 16px;
-    }
+    padding: 0 16px;
 `;
