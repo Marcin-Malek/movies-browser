75180331b2a956cd763ffc488f1355a2f502dcc7
Marcin Malek <marcinmalek01@gmail.com>
2022-07-01 12:43:00 +0200

    created simple slice

---
+++ src/features/movies/moviesSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    moviesList: [],
+    fetchStatus: "initial",
+};
+
+export const moviesSlice = createSlice({
+    name: 'movies',
+    initialState,
+    reducers: {
+        setMoviesList: (state, payload) => {
+            state.moviesList = payload;
+        },
+        startFetching: (state) => {
+            state.fetchStatus = "initiated"
+        },
+        handleFetchingError: (state) => {
+            state.fetchStatus = "error"
+        },
+        finishFetching: (state) => {
+            state.fetchStatus = "completed"
+        },
+        fetchMovies: () => { },
+    },
+});
+
+export const { fetchMovies: fetchPopularMovies } = moviesSlice.actions;
+
+export const selectMovies = (state) => state.movies.moviesList;
+
+export const selectFetchStatus = (state) => state.movies.moviesList;
+
+export default counterSlice.reducer;
