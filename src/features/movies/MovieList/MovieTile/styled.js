12f47ec78f995e55ce1b0ceddb64a377eb9246a3
Damian Michalowski <dowski888@gmail.com>
2022-07-02 14:16:53 +0200

    merge conflict fix, movie list

--- src/features/movies/MovieList/MovieTile/styled.js
+++ src/features/movies/MovieList/MovieTile/styled.js
@@ -8,7 +8,6 @@ export const Content = styled.li`
     border-radius: 5px;
     display: flex;
     flex-direction: column;
-
     @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
         flex-direction: row;
     }
@@ -18,11 +17,9 @@ export const Img = styled.img`
     width: 100%;
     height: 100%;
     border-radius: 5px;
-
     @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px){
         width: 50%;
     }
-
     @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px){
         width: 40%;
     }
@@ -32,7 +29,6 @@ export const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     height: 100%;
-
     @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px){
         margin-left: 16px;
     }
@@ -43,13 +39,10 @@ export const Title = styled.h3`
     font-weight: 500;
     font-size: 22px;
     line-height: 130%;
-
-
     @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
         font-size: 16px;
         margin: 0 0 4px;
     }
-
     @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px){
         font-size: 4vw;
     }
@@ -57,14 +50,11 @@ export const Title = styled.h3`
 
 export const Year = styled.p`
     margin: 0;
-
     line-height: 150%;
     color: ${({ theme }) => theme.color.darkerGray};
-
     @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px){
         font-size: 13px;
     }
-
     @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px){
         font-size: 2.5vw;
     }
@@ -85,13 +75,11 @@ export const Tag = styled.li`
     background-color: ${({ theme }) => theme.color.gray};
     border-radius: 5px;
     font-size: 14px;
-
     
     @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px){
         padding: 4px 8px;
         font-size: 10px;
     }
-
     @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px){
         padding: 1vw 2vw;
         font-size: 2vw;
@@ -116,12 +104,10 @@ export const RatingContainer = styled.div`
 export const StyledStar = styled(Star)`
     width: 24px;
     height: 24px;
-
     @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px){
         width: 16px;
         height: 16px;
     }
-
     @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px){
         width: 3vw;
         height: 3vw;
@@ -130,11 +116,9 @@ export const StyledStar = styled(Star)`
 
 export const Rate = styled.span`
     font-weight: 600;
-
     @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px){
         font-size: 13px;
     }
-
     @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px){
         font-size: 3vw;
     }
@@ -142,11 +126,9 @@ export const Rate = styled.span`
 
 export const Votes = styled.span`
     color: ${({ theme }) => theme.color.darkerGray};
-
     @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px){
         font-size: 13px;
     }
-
     @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px){
         font-size: 3vw;
     }
--- src/features/movies/MovieList/styled.js
+++ src/features/movies/MovieList/styled.js
@@ -1,25 +1,34 @@
 import styled from "styled-components";
 
 export const Content = styled.section`
-    max-width: 1368px;
-    margin: auto;
-    display: flex;
-    flex-direction: column;
+    max-width: 1400px;
+    padding:0 16px;
+    margin: 0 auto;
 `;
 
-export const Title = styled.h1`
-    font-family: 'Poppins';
-    font-style: normal;
-    font-weight: 600;
+export const Title = styled.h2`
     font-size: 36px;
-    line-height: 120%;
     margin: 0;
-    padding: 56px 16px 24px 16px;
+    padding: 56px 0 24px;
+    line-height: 120%;
+    @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
+        font-size:3.51vw;
+    }
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+        font-size: 18px;
+    }
 `;
 
-export const Wrapper = styled.div`
+export const Wrapper = styled.ul`
+    padding: 0;
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(288px, 1fr));
-    gap: 24px;
-    padding: 16px;
+    grid-template-columns: repeat(4, 1fr);
+    gap:24px;
+    @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px){
+        grid-template-columns: 1fr 1fr;
+    }
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px){
+        grid-template-columns: 1fr;
+        gap: 16px;
+    }
 `;
