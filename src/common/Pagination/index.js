4df56906723ad4c1a35351e35388017f175ea4b0
Marcin Malek <marcinmalek01@gmail.com>
2022-07-05 16:06:51 +0200

    made Link work with every path

--- src/common/Pagination/index.js
+++ src/common/Pagination/index.js
@@ -9,18 +9,20 @@ import {
 } from "./styled";
 import { theme } from "../../app/theme.js";
 import { useWindowSize } from "../useWindowSize";
+import { useLocation } from "react-router-dom";
 
 export const Pagination = ({ currentPage, allPages }) => {
     const [width] = useWindowSize();
-    
+    const { pathname } = useLocation();
+
     return (
         <PaginationContainer>
             <ButtonsWrapper disabled={parseInt(currentPage) <= 1}>
-                <Button to={parseInt(currentPage) <= 1 ? "" : `../movies/page=${1}`}>
+                <Button to={parseInt(currentPage) <= 1 ? "" : pathname.replace(currentPage, 1)}>
                     <StyledPrevIcon />
                     {width <= theme.breakpoint.mobile ? <StyledPrevIcon /> : "First"}
                 </Button>
-                <Button to={parseInt(currentPage) <= 1 ? "" : `../movies/page=${parseInt(currentPage) - 1}`}>
+                <Button to={parseInt(currentPage) <= 1 ? "" : pathname.replace(currentPage, parseInt(currentPage) - 1)}>
                     <StyledPrevIcon />
                     {width <= theme.breakpoint.mobile ? "" : "Previous"}
                 </Button>
@@ -31,11 +33,11 @@ export const Pagination = ({ currentPage, allPages }) => {
             </PagesWrapper>
 
             <ButtonsWrapper disabled={parseInt(currentPage) === allPages}>
-                <Button to={parseInt(currentPage) === allPages ? "" : `../movies/page=${parseInt(currentPage) + 1}`}>
+                <Button to={parseInt(currentPage) === allPages ? "" : pathname.replace(currentPage, parseInt(currentPage) + 1)}>
                     {width <= theme.breakpoint.mobile ? "" : "Next"}
                     <StyledNextIcon />
                 </Button>
-                <Button to={parseInt(currentPage) === allPages ? "" : `../movies/page=${allPages}`}>
+                <Button to={parseInt(currentPage) === allPages ? "" : pathname.replace(currentPage, allPages)}>
                     {width <= theme.breakpoint.mobile ? <StyledNextIcon /> : "Last"}
                     <StyledNextIcon />
                 </Button>
