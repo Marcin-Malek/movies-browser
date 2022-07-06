db15a6948dd30146b2879b88c78931b43cac8a99
Damian Michalowski <dowski888@gmail.com>
2022-07-06 14:13:12 +0200

    movieGenres change to moviesGenres

--- src/features/movies/moviesSaga.js
+++ src/features/movies/moviesSaga.js
@@ -13,9 +13,9 @@ function* fetchMoviesHandler() {
     yield put(startFetching());
     try {
         const moviesList = yield axios.get("https://api.themoviedb.org/3/movie/popular?api_key=b6338a2fff00b848e44db36dd695b802&language=en-US&page=1");
-        const movieGenres = yield axios.get("https://api.themoviedb.org/3/genre/movie/list?api_key=b6338a2fff00b848e44db36dd695b802&language=en-US");
+        const moviesGenres = yield axios.get("https://api.themoviedb.org/3/genre/movie/list?api_key=b6338a2fff00b848e44db36dd695b802&language=en-US");
         yield put(setMoviesList(moviesList));
-        yield put(setMoviesGenres(movieGenres));
+        yield put(setMoviesGenres(moviesGenres));
         yield put(finishFetching());
     } catch (error) {
         yield put(handleFetchingError());
