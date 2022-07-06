3aaa19a68c45c1f4823ace45b63a32f9438f6ea4
Damian Michalowski <dowski888@gmail.com>
2022-07-06 18:12:02 +0200

    cleaning code

--- src/features/movies/MovieList/MovieTile/index.js
+++ src/features/movies/MovieList/MovieTile/index.js
@@ -1,8 +1,5 @@
-// import Camera from "../../../images/camera.svg";
-import Poster from "../../../../images/MulanPoster.jpg";
 import {
     Content,
-    Wrapper,
     TagsContainer,
     Tag,
     Title,
