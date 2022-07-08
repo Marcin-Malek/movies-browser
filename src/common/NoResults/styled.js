ccce4a1c117d824e2d0f77be2e519cdb71dd34b6
Damian Michalowski <dowski888@gmail.com>
2022-07-08 12:04:22 +0200

    fixes

--- src/common/NoResults/styled.js
+++ src/common/NoResults/styled.js
@@ -2,38 +2,32 @@ import styled from "styled-components";
 import { ReactComponent as noresults } from "../../images/noresults.svg";
 
 export const Wrapper = styled.div`
-    max-width: 53vw;
-    margin: 56px 0 0 14.3vw;
+    max-width: 1368px;
+    margin: 0 auto;
+    padding: 16px;
     display: flex;
     flex-direction: column;
-
-    @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
-        max-width: 80vw;
-        margin: 50px auto;
-    }
 `;
 
 export const Title = styled.h2`
     font-weight: 600;
-    font-size: 2.23vw;
     line-height: 120%;
-    align-self: flex-start;
-    margin: 0;
+    font-size: 36px;
+    margin-top: 56px;
 
     @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
-        font-size: 6vw;
+        font-size: 3.3vw;
+        margin-top: 4vw;
     }
 `;
 
 export const StyledNoResults = styled(noresults)`
-    align-self: flex-end;
-    width: 34vw;
-    height: 26.5vw;
-
+    width: 668px;
+    height: 508px;
+    align-self: center;
     @media (max-width: ${({ theme }) => theme.breakpoint.tablet}px) {
-        align-self: center;
         width: 68vw;
-        height: 52vw;
+        height: 54vw;
     }
 `;
 
