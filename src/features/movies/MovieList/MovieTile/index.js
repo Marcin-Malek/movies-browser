3bb0348bc7fe59228011d6e2198874c695d8f17a
Marcin Malek <marcinmalek01@gmail.com>
2022-07-12 00:54:26 +0200

    MovieTile code cleanup, useFormatDate

--- src/features/movies/MovieList/MovieTile/index.js
+++ src/features/movies/MovieList/MovieTile/index.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { useFormatDate } from "../../../../common/useFormatDate";
 import {
     Content,
     TagsContainer,
@@ -12,29 +13,44 @@ import {
     FeaturedDate
 } from "./styled";
 
-export const MovieTile = ({ poster, title, date, tags, rate, votes, id, featured }) => {
+export const MovieTile = ({
+    poster,
+    title,
+    date,
+    tags,
+    rate,
+    votes,
+    id,
+    featured }) => {
     const navigate = useNavigate();
+    const year = useFormatDate(date, "year");
 
     return (
         <Content onClick={() => navigate(`../movies/${id}`)}>
             <Img src={poster} alt={title} />
-            <Title> {title} </Title>
+            <Title>{title}</Title>
             <FeaturedDate>
                 {featured && <span>{featured} </span>}
-                {date ? <time dateTime={date}>{featured ? `(${date.slice(0,4)})` : date.slice(0,4)} </time> 
-                : <span>(No release date)</span>}
+                {date ?
+                    <time dateTime={date}>
+                        {featured ? `(${year})` : year}
+                    </time>
+                    :
+                    <span>(No release date)</span>
+                }
             </FeaturedDate>
             <TagsContainer>
                 {tags.map((tag, tagIndex) => (
                     <Tag key={tagIndex}>{tag}</Tag>
                 ))}
             </TagsContainer>
-
-            {(votes && rate && <RatingContainer>
-                <StyledStar />
-                <Rate>{rate}</Rate>
-                <Votes>{votes} votes</Votes>
-            </RatingContainer>) || null}
+            {(votes && rate &&
+                <RatingContainer>
+                    <StyledStar />
+                    <Rate>{rate}</Rate>
+                    <Votes>{votes} votes</Votes>
+                </RatingContainer>
+            ) || null}
         </Content>
     )
 };
