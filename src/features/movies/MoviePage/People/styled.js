b9c199e8a468585b9712ca5145d259c56b87fd94
Damian Michalowski <dowski888@gmail.com>
2022-07-12 14:27:59 +0200

    added cast i crew in movie detail, fixing styles in peson list

--- src/features/movies/MoviePage/People/styled.js
+++ src/features/movies/MoviePage/People/styled.js
@@ -13,21 +13,16 @@ export const Header = styled.header`
 
 export const List = styled.ul`
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(208px, 1fr));
+    /* grid-template-columns: repeat(auto-fit, minmax(208px, 1fr)); */
+    grid-template-columns: repeat(auto-fit,208px);
+    justify-content: center;
     list-style-type: none;
     padding: 0;
     gap: 24px;
 
     @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-        grid-template-columns: repeat(2, 1fr);
-    }
-`;
-
-export const Item = styled.li`
-    height: 339px;
-    background-color: ${({theme}) => theme.color.white};
-    
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-        height: 245px;
+        /* grid-template-columns: repeat(2, 1fr); */
+        grid-template-columns: repeat(auto-fit,136px);
+        gap: 16px;
     }
 `;
