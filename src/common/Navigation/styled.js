1e4570223496c10f495888bac8e58152b10524b8
Marcin Malek <marcinmalek01@gmail.com>
2022-07-14 22:55:15 +0200

    added breakpoint styles, further styling improvement

--- src/common/Navigation/styled.js
+++ src/common/Navigation/styled.js
@@ -13,8 +13,14 @@ export const NavbarContainer = styled.nav`
     width: 1368px;
     display: grid;
     grid-template-columns: 1fr 1fr 2.5fr;
-    padding: 27px 16px;
+    padding: 23px 16px;
     align-items: center;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+        grid-template-columns: 1fr 1fr;
+        grid-template-rows: auto;
+        grid-row-gap: 32px;
+    }
 `;
 
 /* export const NavbarWrapper = styled.div`
@@ -33,7 +39,8 @@ export const Logotype = styled(Link)`
     text-decoration: none;
     
     @media(max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-        margin-bottom: 18px;
+        grid-row: 1;
+        grid-column: 1;
     }
     
     &:hover {
@@ -84,10 +91,13 @@ export const StyledNavbar = styled.ul`
     display: flex;
     align-items: center;
     padding: 0;
-    margin: 16px;
+    margin: 0 16px;
 
     @media(max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-        margin: 16px 28px 6px 0;
+        grid-row: 1;
+        grid-column: 2;
+        margin: 0;
+        justify-self: end;
     }
 `;
 
@@ -98,13 +108,12 @@ export const StyledNavLink = styled(NavLink)`
     font-size: 14px;
     line-height: 21px;
     border-radius: 24px;
-    
+
     &.active {
         border: 1px solid;
     }
 
     @media(max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-        display: block;
         padding: 8px 12px;
         font-size: 12px;
         line-height: 18px;
@@ -121,7 +130,7 @@ export const StyledSearchBox = styled.input`
     text-indent: 60px;
 
     background-image: url(${img});
-    background-size: 20px 20px;
+    background-size: 20px;
     background-repeat: no-repeat;
     background-position: 25px center;
 
@@ -130,14 +139,13 @@ export const StyledSearchBox = styled.input`
     }
 
     @media(max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-        width: 90vw;
+        grid-column: 1/3;
+        max-width: unset;
         height: 44px;
         text-indent: 40px;
         background-position: 17px center;
-        background-size: 14px 14px;
+        background-size: 14px;
         font-size: 13px;
-        display: flex;
-        flex-grow: 1;
     }
 `;
 
