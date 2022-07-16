a0b21c1c6d2f6245f43b4937258a7f2eaf0eb48b
Damian Michalowski <dowski888@gmail.com>
2022-07-16 10:35:33 +0200

    people grid style fix, scaling image fix

--- src/features/people/PeopleList/styled.js
+++ src/features/people/PeopleList/styled.js
@@ -3,13 +3,8 @@ import styled from "styled-components";
 export const Wrapper = styled.ul`
     padding: 0;
     display: grid;
-    grid-template-columns: repeat(5, 1fr);
+    grid-template-columns: repeat(auto-fill, minmax(208px, 1fr));
     gap:24px;
-
-    @media (max-width: ${({theme}) => theme.breakpoint.tablet}px){
-        grid-template-columns: repeat(4, 1fr);
-        gap: 16px;
-    }
 
     @media (max-width: ${({theme}) => theme.breakpoint.mobile}px){
         grid-template-columns: repeat(2, 1fr);
